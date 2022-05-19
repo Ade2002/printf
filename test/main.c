dad22d7c3e1bd9a30f5b629893c4e73f45dce8b2
oolufolabii <otitoola.olufolabi@gmail.com>
2022-05-19 19:41:38 +0100

    Printf function

---
+++ test/main.c
@@ -0,0 +1,13 @@
+#include "../main.h"
+
+int main(void)
+{
+    int n;
+    _printf("10 in binary is %b, in octal is %o and hexa %x student\n", 10, 10, 11);
+    _printf("%x student\n", 2545);
+    printf("%X student\n", 2545);
+    printf("%x student\n", 2545);
+
+    printf("10 in binary is %b, in octal is %o\n", 10, 10);
+    
+}
