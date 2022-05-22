5ecb5847d47a7f8d3f10a0c6a775568d3fb4ef5d
oolufolabii <otitoola.olufolabi@gmail.com>
2022-05-22 14:37:43 +0100

    printf project

---
+++ test/main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *main - carry out the test
+ *Return: Return 0
+ */
+int main(void)
+{
+_printf("Hello in reversed format is %r\n", "Hello");
+_printf("Character:[%c]\n", 'H');
+printf("Character:[%c]\n", 'H');
+_printf("String:[%s]\n", "I am a string !");
+printf("String:[%s]\n", "I am a string !");
+_printf("String:[%X]\n", 200);
+printf("String:[%X]\n", 200);
+_printf("10 in Hexadecimal:[%X]\n", 10);
+printf("10 in Hexadecimal:[%X]\n", 10);
+
+return (0);
+}
